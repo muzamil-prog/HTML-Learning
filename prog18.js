390d90e2c1ff288c6628896e0155008584415b8e
Muzammil Hussain <muzammil.org73@gmail.com>
2026-05-18 23:55:17 +0500

    Add files via upload

---
+++ prog18.js
@@ -0,0 +1,21 @@
+console.log("Finde the sum of to number ");
+function sum(a, b){
+    console.log(a+b);
+    return sum;
+}
+result=sum(5,8);
+result=sum(5,5);
+result=sum(5,8
+);
+result=sum(5,9);
+
+
+
+//aero function   
+
+
+ const fun= x=>{
+    console.log("i am an arroq function", x)
+}
+ fun(34);
+ fun(56);
